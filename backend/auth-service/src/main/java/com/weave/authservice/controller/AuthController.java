27861d23b855979bb939c639f56a29142dd347e5
eun61n00 <eun61n@gmail.com>
2025-09-22 00:01:29 +0900

    feat(auth): 인증 구현(#14)

---
+++ backend/auth-service/src/main/java/com/weave/authservice/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.weave.authservice.controller;
+
+import com.weave.authservice.dto.request.LoginRequest;
+import com.weave.authservice.dto.response.LoginResponse;
+import com.weave.authservice.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        LoginResponse loginResponse = authService.login(request);
+        return ResponseEntity.ok(loginResponse);
+    }
+
+}
