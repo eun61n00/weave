8ddb23f5e635dcd9992547714ac96d970263e9c7
eun61n00 <eun61n@gmail.com>
2025-10-15 11:25:36 +0900

    feat(auth): DTO, entity 수정(#28)

--- backend/auth-service/src/main/java/com/weave/authservice/dto/request/RefreshTokenRequest.java
+++ backend/auth-service/src/main/java/com/weave/authservice/dto/request/RefreshTokenRequest.java
@@ -1,6 +1,5 @@
 package com.weave.authservice.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,12 +7,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UpdateRefreshTokenRequest {
+public class RefreshTokenRequest {
 
-    @NotBlank
-    private Long userId;
-
-    @NotBlank
     private String refreshToken;
 
 }
---
+++ backend/auth-service/src/main/java/com/weave/authservice/dto/response/AccessTokenResponse.java
@@ -0,0 +1,12 @@
+package com.weave.authservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccessTokenResponse {
+    private String accessToken;
+}
