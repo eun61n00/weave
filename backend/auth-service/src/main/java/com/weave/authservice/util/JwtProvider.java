64ca1f55d1b188d72d48cefc3aa9ddee211be6c0
eun61n00 <eun61n@gmail.com>
2025-10-17 11:36:37 +0900

    feat(auth): 토큰 검증 로직 추가(#28)

--- backend/auth-service/src/main/java/com/weave/authservice/util/JwtProvider.java
+++ backend/auth-service/src/main/java/com/weave/authservice/util/JwtProvider.java
@@ -1,17 +1,21 @@
 package com.weave.authservice.util;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.weave.authservice.exception.WeaveAuthException;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
+import static com.weave.authservice.exception.ErrorMap.*;
+
+@Slf4j
 @Component
 public class JwtProvider {
 
@@ -20,9 +24,11 @@ public class JwtProvider {
     @Value("${jwt.secret.key}")
     private String secretKey;
 
+    @Getter
     @Value("${jwt.access.token.valid.time}")
     private long accessTokenValidTime;
 
+    @Getter
     @Value("${jwt.refresh.token.valid.time}")
     private long refreshTokenValidTime;
 
@@ -51,4 +57,39 @@ public class JwtProvider {
                 .compact();
     }
 
+    public void validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+            throw new WeaveAuthException(TOKEN_INVALID_ERROR);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+            throw new WeaveAuthException(TOKEN_EXPIRED_ERROR);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+            throw new WeaveAuthException(TOKEN_INVALID_ERROR);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+            throw new WeaveAuthException(TOKEN_INVALID_ERROR);
+        }
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    public Long getUserIdFromToken(String token) {
+        return parseClaims(token).get("userId", Long.class);
+    }
+
+    public Long getExpiration(String token) {
+        Date expiration = parseClaims(token).getExpiration();
+        long now = new Date().getTime();
+        return (expiration.getTime() - now);
+    }
 }
